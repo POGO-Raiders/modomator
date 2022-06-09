255480d1970412a9dea3d310a5cc9b21faf8e3e0
cheopsrule <blake.heyman@gmail.com>
2022-06-09 20:56:58 +0200

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import "./App.css";
 import React from "react";
-import { Layout } from "antd";
+import { Layout, Typography } from "antd";
 import ModForm from "./ModForm";
+import logo from "./pgricon64.png";
 
-const { Footer } = Layout;
+const { Footer, Header } = Layout;
 
 const App = () => {
   return (
-    <Layout>
+    <Layout style={{ background: "none" }}>
+      <Header style={{ background: "none" }}>
+        <h1 className="centered form-title">Modomator</h1>
+      </Header>
       <ModForm />
-      <Footer style={{ textAlign: "center" }}>Pokémon GO Raiders</Footer>
+      <Footer style={{ textAlign: "center", background: "none" }}>
+        <img width={32} src={logo} alt="Pokémon Go Raiders logo" style={{ marginRight: 8 }} />
+        Pokémon GO Raiders
+      </Footer>
     </Layout>
   );
 };

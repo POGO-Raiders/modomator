d3e6c3d36760ce20ab0f921428a1304d1da7bf9c
cheopsrule <blake.heyman@gmail.com>
2022-06-12 21:58:50 +0200

    Working menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import "./App.css";
 import React from "react";
-import { Layout } from "antd";
+import { Button, Layout, Popover } from "antd";
 import ModForm from "./ModForm";
 import SettingsMenu from "./SettingsMenu";
+import { SettingOutlined } from "@ant-design/icons";
 const logo = require("./assets/pgricon64.png");
 
 const { Footer, Header } = Layout;
@@ -12,7 +13,13 @@ const App = (): JSX.Element => {
     <Layout style={{ background: "none" }}>
       <Header style={{ background: "none" }}>
         <h1 className="centered form-title">Modomator</h1>
-        <SettingsMenu className="home-settings" />
+        <Popover content={<SettingsMenu />} trigger="click" placement="bottomRight">
+          <Button
+            shape="circle"
+            icon={<SettingOutlined />}
+            style={{ position: "absolute", right: 18, top: 18 }}
+          />
+        </Popover>
       </Header>
       <ModForm />
       <Footer style={{ textAlign: "center", background: "none" }}>
--- src/SettingsMenu.tsx
+++ src/SettingsMenu.tsx
@@ -2,11 +2,11 @@ import useLocalStorage from "use-local-storage";
 import { Col, Row, Switch } from "antd";
 import React from "react";
 
-const SettingsMenu = ({ className }: { className: string }): JSX.Element => {
+const SettingsMenu = (): JSX.Element => {
   const [openInDiscord, setOpenInDiscord] = useLocalStorage("openInDiscord", true);
 
   return (
-    <div className={className}>
+    <div>
       <Row align="middle" gutter={[10, 0]}>
         <Col>Open in Discord</Col>
         <Col>

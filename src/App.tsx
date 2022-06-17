50ba133005719ccefca96babe48ae4a96979220c
cheopsrule <blake.heyman@gmail.com>
2022-06-18 01:32:58 +0200

    Install router

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,41 @@ import "./App.css";
 import React from "react";
 import { Button, Layout, Popover } from "antd";
 import ModForm from "./ModForm";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import SettingsMenu from "./SettingsMenu";
 import { SettingOutlined } from "@ant-design/icons";
+import Heatran from "./Heatran";
 const logo = require("./assets/pgricon64.png");
 
 const { Footer, Header } = Layout;
 
 const App = (): JSX.Element => {
   return (
-    <Layout style={{ background: "none" }}>
-      <Header style={{ background: "none" }}>
-        <h1 className="centered form-title">Modomator</h1>
-        <Popover content={<SettingsMenu />} trigger="click" placement="bottomRight">
-          <Button
-            shape="circle"
-            icon={<SettingOutlined />}
-            style={{ position: "absolute", right: 18, top: 18 }}
-          />
-        </Popover>
-      </Header>
-      <ModForm />
-      <Footer style={{ textAlign: "center", background: "none" }}>
-        <img width={32} src={logo} alt="Pokémon Go Raiders logo" style={{ marginRight: 8 }} />
-        Pokémon GO Raiders
-      </Footer>
-    </Layout>
+    <Router>
+      <Layout style={{ background: "none" }}>
+        <Header style={{ background: "none" }}>
+          <h1 className="centered form-title">Modomator</h1>
+          <Popover content={<SettingsMenu />} trigger="click" placement="bottomRight">
+            <Button
+              shape="circle"
+              icon={<SettingOutlined />}
+              style={{ position: "absolute", right: 18, top: 18 }}
+            />
+          </Popover>
+        </Header>
+
+        <Routes>
+          <Route path="/modomator" element={<ModForm />} />
+          <Route path="/modomator/changelog" element={<ModForm />} />
+          <Route path="*" element={<Heatran />} />
+        </Routes>
+
+        <Footer style={{ textAlign: "center", background: "none" }}>
+          <img width={32} src={logo} alt="Pokémon Go Raiders logo" style={{ marginRight: 8 }} />
+          Pokémon GO Raiders
+        </Footer>
+      </Layout>
+    </Router>
   );
 };
 

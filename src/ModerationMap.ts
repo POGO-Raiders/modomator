ec57991bf5371b0d34d32336ee922826de9fa44a
cheopsrule <blake.heyman@gmail.com>
2022-06-19 22:15:01 +0200

    Stop pinging Moderator

--- src/ModerationMap.ts
+++ src/ModerationMap.ts
@@ -2,7 +2,7 @@ const ModerationMap: { [reason: string]: string } = {
   "Discrimination": "Discrimination is not allowed on this server and likely violates Discord ToS.",
   "Failure to commit": "When you react to a raid, you are expected to join it. Please review the <#736807346268930058> of the server and <#793767024262119433>.",
   "Failure to invite": "Failure to invite all members of your party to the raid. Make sure to use private lobbies and the search string below the QR code. Please review <#793767024262119433> and the server’s <#736807346268930058> for more information.",
-  "Failure to reattempt": "When you host a raid that’s unsuccessful, you are expected to continue communicating with your lobby and provide a reattempt to prevent lost passes. If you need help, ping @Moderator. Please review <#793767024262119433> and the server’s <#736807346268930058> for more information.",
+  "Failure to reattempt": "When you host a raid that’s unsuccessful, you are expected to continue communicating with your lobby and provide a reattempt to prevent lost passes. If you need help, ping `@Moderator`. Please review <#793767024262119433> and the server’s <#736807346268930058> for more information.",
   "Harassment": "Harassment is not allowed on this server and likely violates Discord ToS.",
   "Less than 10 minutes": "Hosting with less than ten minutes on the raid clock. This endangers the other members' passes. Please review the server’s <#736807346268930058> and <#793767024262119433> for more information.",
   "Moderation Evasion": "You may not leave the server to avoid moderation.",

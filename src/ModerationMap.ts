1d005cdf25f2e38c154eb2d8b9e3e2590239fd55
cheopsrule <blake.heyman@gmail.com>
2022-06-19 17:16:22 +0200

    Update ModerationMap.ts

--- src/ModerationMap.ts
+++ src/ModerationMap.ts
@@ -1,20 +1,17 @@
 const ModerationMap: { [reason: string]: string } = {
-  Discrimination: "Discrimination is not allowed on this server, and violates Discord TOS.",
-  "Failure to commit":
-    "Failing to commit to a raid. Rule 8. When you react to a raid, you are expected to join it. Please review the #rules of the server and #how-to-raid.",
-  "Failure to invite":
-    "Failure to invite all members of your party to the raid. Make sure to use private lobbies and the search string below the QR code. Please review #how-to-raid and the server’s #rules for more information.",
-  "Failure to reattempt":
-    "Violating Rule 14: Failure to reattempt a raid. When you host a raid that’s unsuccessful, you are expected to continue communicating with your lobby and provide a reattempt for them should it fail, to prevent members from losing their passes. If you need help, ping @Moderator. Please review #how-to-raid and the server’s #rules for more information.",
-  "Less than 10 minutes":
-    "Violating Rule 12: Hosting with less than ten minutes on the raid clock. This endangers the other member’s passes. Please review the server’s #rules and #how-to-raid for more information.",
-  NSFW: "Posting NSFW content is not allowed on this server, and violates Discord TOS.",
-  Spamming: "Spamming.",
-  Spoofing: "Spoofing is not tolerated on this server, and violates Niantic’s TOS.",
-  "Troll hosting":
-    "Violating Rule 13: Troll hosting. When you post a raid, be prepared to host it.",
-  "Unhatched Egg":
-    "Violating Rule 13: Failure to host from a hatched egg. Hosts must be within range of a gym and ready to host and the egg must be already hatched before posting a raid to host. Please review the #rules of the server and #how-to-raid.",
+  "Discrimination": "Discrimination is not allowed on this server and likely violates Discord ToS.",
+  "Failure to commit": "When you react to a raid, you are expected to join it. Please review the <#736807346268930058> of the server and <#793767024262119433>.",
+  "Failure to invite": "Failure to invite all members of your party to the raid. Make sure to use private lobbies and the search string below the QR code. Please review <#793767024262119433> and the server’s <#736807346268930058> for more information.",
+  "Failure to reattempt": "When you host a raid that’s unsuccessful, you are expected to continue communicating with your lobby and provide a reattempt to prevent lost passes. If you need help, ping @Moderator. Please review <#793767024262119433> and the server’s <#736807346268930058> for more information.",
+  "Harassment": "Harassment is not allowed on this server and likely violates Discord ToS.",
+  "Less than 10 minutes": "Hosting with less than ten minutes on the raid clock. This endangers the other members' passes. Please review the server’s <#736807346268930058> and <#793767024262119433> for more information.",
+  "Moderation Evasion": "You may not leave the server to avoid moderation.",
+  "NSFW": "Posting NSFW content is not allowed on this server and likely violates Discord ToS.",
+  "Promotion": "You may not promote unaffiliated links, resources, or servers on PGR.",
+  "Spamming": "Spamming.",
+  "Spoofing": "Spoofing is not tolerated on this server and violates Niantic’s ToS.",
+  "Troll hosting": "Troll hosting. When you post a raid, be prepared to host it.",
+  "Unhatched Egg": "Failure to host from a hatched egg. Hosts must be within range of a gym and the egg must be already hatched before posting a raid to host. Please review the <#736807346268930058> of the server and <#793767024262119433>.",
 };
 
 export default ModerationMap;

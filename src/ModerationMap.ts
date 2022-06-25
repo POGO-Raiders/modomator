13201db237685f6081a77f2ac263283019c2edda
cheopsrule <blake.heyman@gmail.com>
2022-06-26 01:11:00 +0200

    Rework map structure

--- src/ModerationMap.ts
+++ src/ModerationMap.ts
@@ -1,17 +1,80 @@
-const ModerationMap: { [reason: string]: string } = {
-  "Discrimination": "Discrimination is not allowed on this server and likely violates Discord ToS.",
-  "Failure to commit": "Failure to commit to a raid. When you react to a raid, you are expected to join it. Please review the <#736807346268930058> of the server and <#793767024262119433>.",
-  "Failure to invite": "Failure to invite all members of your party to the raid. Make sure to use private lobbies and the search string below the QR code. Please review <#793767024262119433> and the server’s <#736807346268930058> for more information.",
-  "Failure to reattempt": "When you host a raid that’s unsuccessful, you are expected to continue communicating with your lobby and provide a reattempt to prevent lost passes. If you need help, ping `@Moderator`. Please review <#793767024262119433> and the server’s <#736807346268930058> for more information.",
-  "Harassment": "Harassment is not allowed on this server and likely violates Discord ToS.",
-  "Less than 10 minutes": "Hosting with less than ten minutes on the raid clock. This endangers the other members' passes. Please review the server’s <#736807346268930058> and <#793767024262119433> for more information.",
-  "Moderation Evasion": "You may not leave the server to avoid moderation.",
-  "NSFW": "Posting NSFW content is not allowed on this server and likely violates Discord ToS.",
-  "Promotion": "You may not promote unaffiliated links, resources, or servers on PGR.",
-  "Spamming": "Spamming.",
-  "Spoofing": "Spoofing is not tolerated on this server and violates Niantic’s ToS.",
-  "Troll hosting": "Troll hosting. When you post a raid, be prepared to host it.",
-  "Unhatched Egg": "Failure to host from a hatched egg. Hosts must be within range of a gym and the egg must be already hatched before posting a raid to host. Please review the <#736807346268930058> of the server and <#793767024262119433>.",
+import { ModerationAction } from "./Moderation";
+
+const ModerationMap: {
+  [reason: string]: { description: string; hosting: boolean; categories: ModerationAction[] };
+} = {
+  Discrimination: {
+    description: "Discrimination is not allowed on this server and likely violates Discord ToS.",
+    hosting: false,
+    categories: [ModerationAction.Ban, ModerationAction.Mute, ModerationAction.Warning],
+  },
+  "Failure to commit": {
+    description:
+      "Failure to commit to a raid. When you react to a raid, you are expected to join it. Please review the <#736807346268930058> of the server and <#793767024262119433>.",
+    hosting: false,
+    categories: [ModerationAction.Warning],
+  },
+
+  "Failure to invite": {
+    description:
+      "Failure to invite all members of your party to the raid. Make sure to use private lobbies and the search string below the QR code. Please review <#793767024262119433> and the server’s <#736807346268930058> for more information.",
+    hosting: true,
+    categories: [ModerationAction.Warning],
+  },
+  "Failure to reattempt": {
+    description:
+      "When you host a raid that’s unsuccessful, you are expected to continue communicating with your lobby and provide a reattempt to prevent lost passes. If you need help, ping `@Moderator`. Please review <#793767024262119433> and the server’s <#736807346268930058> for more information.",
+    hosting: true,
+    categories: [ModerationAction.Warning],
+  },
+  Harassment: {
+    description: "Harassment is not allowed on this server and likely violates Discord ToS.",
+    hosting: false,
+    categories: [ModerationAction.Warning],
+  },
+  "Less than 10 minutes": {
+    description:
+      "Hosting with less than ten minutes on the raid clock. This endangers the other members' passes. Please review the server’s <#736807346268930058> and <#793767024262119433> for more information.",
+    hosting: true,
+    categories: [ModerationAction.Warning],
+  },
+  "Moderation Evasion": {
+    description: "You may not leave the server to avoid moderation.",
+    hosting: false,
+    categories: [ModerationAction.Ban],
+  },
+  NSFW: {
+    description:
+      "Posting NSFW content is not allowed on this server and likely violates Discord ToS.",
+    hosting: false,
+    categories: [ModerationAction.Mute, ModerationAction.Warning, ModerationAction.Ban],
+  },
+  Promotion: {
+    description: "You may not promote unaffiliated links, resources, or servers on PGR.",
+    hosting: false,
+    categories: [ModerationAction.Mute, ModerationAction.Warning, ModerationAction.Ban],
+  },
+  Spamming: {
+    description: "Spamming and disrupting the chat. Please take some time to re-read <#736807346268930058> before participating on the server again.",
+    hosting: false,
+    categories: [ModerationAction.Mute, ModerationAction.Warning, ModerationAction.Ban],
+  },
+  Spoofing: {
+    description: "Spoofing is not tolerated on this server and violates Niantic’s ToS.",
+    hosting: false,
+    categories: [ModerationAction.Ban],
+  },
+  "Troll hosting": {
+    description: "Troll hosting. When you post a raid, be prepared to host it.",
+    hosting: true,
+    categories: [ModerationAction.Warning, ModerationAction.Ban],
+  },
+  "Unhatched Egg": {
+    description:
+      "Failure to host from a hatched egg. Hosts must be within range of a gym and the egg must be already hatched before posting a raid to host. Please review the <#736807346268930058> of the server and <#793767024262119433>.",
+    hosting: true,
+    categories: [ModerationAction.Warning],
+  },
 };
 
 export default ModerationMap;
